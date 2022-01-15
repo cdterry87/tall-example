f8a01161807975bbbdb51f9c86b4936c9ad47d51
Chase Terry <chase.terry87@gmail.com>
2022-01-15 14:09:32 -0600

    Created app layout component

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+    >
+
+    <title>TALL Example</title>
+
+    <!-- Styles -->
+    <link
+        rel="stylesheet"
+        href="{{ mix('css/app.css') }}"
+    >
+
+    @livewireStyles
+
+    <!-- Scripts -->
+    <script
+        src="{{ mix('js/app.js') }}"
+        defer
+    ></script>
+</head>
+
+<body class="antialiased">
+
+    {{-- form sidebar --}}
+    @if (isset($form) && !$form->isEmpty())
+        <x-form>
+            {{ $form }}
+        </x-form>
+    @endif
+    {{-- end form sidebar --}}
+
+    {{-- main content --}}
+    <div class="bg-gray-100 h-screen p-8">
+        {{ $slot }}
+    </div>
+    {{-- end main content --}}
+
+    @livewireScripts
+</body>
+
+</html>
